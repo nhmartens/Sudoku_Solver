122d670bd3be0c8309687e67f3778facfe076143
nhmartens <nils.martens@bluewin.ch>
2024-01-06 15:21:33 +0100

    Update

--- main.py
+++ main.py
@@ -6,9 +6,15 @@ if __name__ == "__main__":
     solvedPath = "./Solutions_Sudokus"
     
 
-    allowed_extensions = ['.png', '.jpeg']
+    allowedExtensions = ['.png', '.jpeg']
 
     # Filter Folder files for pngs and jpegs
-    files = [f for f in os.listdir(unsolvedPath) if os.path.isfile(os.path.join(unsolvedPath, f)) and any(f.lower().endswith(ext) for ext in allowed_extensions)]
+    unsolvedFiles = [f for f in os.listdir(unsolvedPath) if os.path.isfile(os.path.join(unsolvedPath, f)) and any(f.lower().endswith(ext) for ext in allowedExtensions)]
+    solvedFiles = [f.replace("solution_", "") for f in os.listdir(solvedPath) if os.path.isfile(os.path.join(solvedPath, f)) and any(f.lower().endswith(ext) for ext in allowedExtensions)]
     
+    filesToSolve = [f for f in unsolvedFiles if f not in solvedFiles]
     
+    for file in filesToSolve:
+        filePath = os.path.join(unsolvedPath, file)
+        imageSolver.solveImage(filePath)
+        
